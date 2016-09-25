269ef7e9880b7b0772b4c37cf9d7f70a56f7c7cc
Piotr Szymanski <szyman@magres.net>
2016-09-25 19:16:49 +0200

    Use Hazelcast

--- image-service/src/main/java/com/metapx/server/ImageService/App.java
+++ image-service/src/main/java/com/metapx/server/ImageService/App.java
@@ -1,6 +1,11 @@
 package com.metapx.server.ImageService;
 
+import com.hazelcast.config.Config;
+
 import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
+import io.vertx.core.spi.cluster.ClusterManager;
+import io.vertx.spi.cluster.hazelcast.HazelcastClusterManager;
 
 /**
  * Hello world!
@@ -12,6 +17,17 @@ public class App {
     System.out.println("Hello World!");
     //Vertx.vertx().createHttpServer().requestHandler(req -> req.response().end("Hello World!")).listen(8080);
     
-    Vertx.vertx().deployVerticle(RestVerticle.class.getName());
+    Config hazelcastConfig = new Config();
+    hazelcastConfig.getNetworkConfig().getJoin().getTcpIpConfig().addMember("127.0.0.1").setEnabled(true);
+    hazelcastConfig.getNetworkConfig().getJoin().getMulticastConfig().setEnabled(false);
+
+    ClusterManager mgr = new HazelcastClusterManager(hazelcastConfig);
+    VertxOptions options = new VertxOptions().setClusterManager(mgr);
+    Vertx.clusteredVertx(options, res -> {
+        if (res.succeeded()) {
+            Vertx vertx = res.result();
+            vertx.deployVerticle(RestVerticle.class.getName());
+        }
+    });
   }
 }
