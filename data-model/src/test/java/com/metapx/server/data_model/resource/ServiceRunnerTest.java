eb24359d134e1bcc4189b7f8c8216fcd8e9dfec0
Piotr Szymanski <szyman@magres.net>
2016-10-16 21:19:14 +0200

    Not found tests

--- data-model/src/test/java/com/metapx/server/data_model/resource/ServiceRunnerTest.java
+++ data-model/src/test/java/com/metapx/server/data_model/resource/ServiceRunnerTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 
 import com.metapx.server.data_model.BaseDatabaseTest;
 import com.metapx.server.data_model.domain.User;
+import com.metapx.server.data_model.resource.infrastructure.CrudError;
 import com.metapx.server.data_model.resource.infrastructure.Resource;
 import com.metapx.server.data_model.resource.infrastructure.ServiceRunner;
 
@@ -20,10 +21,20 @@ public class ServiceRunnerTest extends BaseDatabaseTest {
   }
 
   @Test
-  public void testRead() {
+  public void testReadExisting() {
     Resource<User> result = runner.read("1", this.dslContext);
     
     assertEquals("http://example.org/user/1", result.getCanonicalUrl());
     assertEquals("Administrator", result.getRepresentation().getHandle());
+  }
+
+  @Test(expected = CrudError.class)
+  public void testReadInvalidFormat() {
+    runner.read("qwe", this.dslContext);
+  }
+
+  @Test(expected = CrudError.class)
+  public void testReadNotFound() {
+    runner.read("234", this.dslContext);
   }
 }
