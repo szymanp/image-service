36de7ecbf4cc62579c158271c0146e5a6fa30c60
Piotr Szymanski <szyman@magres.net>
2017-05-19 15:50:09 +0200

    Remove unused import.

--- local-picture-repo/src/main/java/com/metapx/local_picture_repo/scaling/ScaledPictureProvider.java
+++ local-picture-repo/src/main/java/com/metapx/local_picture_repo/scaling/ScaledPictureProvider.java
@@ -3,7 +3,6 @@ package com.metapx.local_picture_repo.scaling;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.Future;
 
