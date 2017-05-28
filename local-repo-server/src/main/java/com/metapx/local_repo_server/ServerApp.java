032db3796f7b1fd9c9608fd6564d367768c8fa75
Piotr Szymanski <szyman@magres.net>
2017-05-28 13:08:03 +0200

    Start LocalRepoServer with HazelCast cluster.

--- local-repo-server/src/main/java/com/metapx/local_repo_server/ServerApp.java
+++ local-repo-server/src/main/java/com/metapx/local_repo_server/ServerApp.java
@@ -1,10 +1,24 @@
 package com.metapx.local_repo_server;
 
+import com.hazelcast.config.Config;
+
+import io.vertx.core.VertxOptions;
+import io.vertx.core.spi.cluster.ClusterManager;
 import io.vertx.rxjava.core.Vertx;
+import io.vertx.spi.cluster.hazelcast.HazelcastClusterManager;
 
 public class ServerApp {
   public static void main(String[] args) {
-    final Vertx vertx = Vertx.vertx();
-    vertx.deployVerticle(ServerVerticle.class.getName());
+    final Config hazelcastConfig = new Config();
+    hazelcastConfig.getNetworkConfig().getJoin().getMulticastConfig().setEnabled(true);
+
+    ClusterManager mgr = new HazelcastClusterManager(hazelcastConfig);
+    VertxOptions options = new VertxOptions().setClusterManager(mgr);
+    Vertx.clusteredVertx(options, res -> {
+      if (res.succeeded()) {
+        Vertx vertx = res.result();
+        vertx.deployVerticle(ServerVerticle.class.getName());
+      }
+    });
   }
 }
